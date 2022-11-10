de787923551316d1bb1d67029edb0cace743353b
shurik9903 <59256714+shurik9903@users.noreply.github.com>
2022-11-10 22:30:36 +0700

    10.11.22_2

--- lib/pages/WEBDialog.dart
+++ lib/pages/WEBDialog.dart
@@ -1,10 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_proj/theme/AppThemeDefault.dart';
+import 'package:provider/provider.dart';
 
 import 'dart:html';
+import 'package:http/http.dart' as http;
 import 'dart:ui' as ui;
 
 import 'package:webviewx/webviewx.dart';
+
+class SelectText extends ChangeNotifier {
+  String _text = "";
+
+  set Text(String text) {
+    _text = text;
+    notifyListeners();
+  }
+
+  String get Text => _text;
+}
+
+class SelectUrl extends ChangeNotifier {
+  String _url = "";
+
+  set Url(String url) {
+    _url = url;
+    notifyListeners();
+  }
+
+  String get Url => _url;
+}
 
 Future<void> showDialogWindow(BuildContext context) async {
   return await showDialog(
@@ -23,116 +47,103 @@ class URLDialog extends StatefulWidget {
 }
 
 class _URLDialogState extends State<URLDialog> {
-  String? urlInput = "", url = "";
+  String? urlInput = "";
+  final SelectText _selectText = SelectText();
+  final SelectUrl _selectUrl = SelectUrl();
 
   @override
   Widget build(BuildContext context) {
     return Dialog(
-      child: Stack(
-        children: [
-          SizedBox(
-            width: double.infinity,
-            child: Column(
-              children: [
-                const Text("Просмотр сайта"),
-                Row(
+      child: MultiProvider(
+        providers: [
+          ChangeNotifierProvider(
+            create: (context) => _selectText,
+          ),
+          ChangeNotifierProvider(
+            create: (context) => _selectUrl,
+          )
+        ],
+        builder: (context, child) {
+          return Stack(
+            // child: Stack(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                child: Column(
                   children: [
-                    Container(
-                      margin: const EdgeInsets.symmetric(horizontal: 10),
-                      alignment: Alignment.center,
-                      child: const Text("URL: "),
+                    const Text("Просмотр сайта"),
+                    Row(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.symmetric(horizontal: 10),
+                          alignment: Alignment.center,
+                          child: const Text(
+                            "URL: ",
+                            maxLines: null,
+                          ),
+                        ),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(right: 20),
+                            child: TextField(
+                              onChanged: (value) {
+                                setState(() {
+                                  urlInput = value;
+                                });
+                              },
+                            ),
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.only(right: 20),
+                          child: IconButton(
+                            onPressed: () {
+                              context.read<SelectUrl>().Url = urlInput ?? "";
+                            },
+                            icon: const Icon(Icons.arrow_circle_right_outlined),
+                          ),
+                        )
+                      ],
                     ),
                     Expanded(
                       child: Container(
-                        margin: const EdgeInsets.only(right: 10),
-                        child: TextField(
-                          onChanged: (value) {
-                            setState(() {
-                              urlInput = value;
-                            });
-                          },
-                        ),
+                        margin: const EdgeInsets.all(10),
+                        color: appTheme(context).mColor1,
+                        height: double.infinity,
+                        width: double.infinity,
+                        child: const WebView(),
                       ),
+                    ),
+                    Container(
+                      color: appTheme(context).mColor2,
+                      height: 200,
+                      width: double.infinity,
+                      padding: const EdgeInsets.symmetric(
+                        horizontal: 10,
+                        vertical: 5,
+                      ),
+                      margin: const EdgeInsets.all(10),
+                      child: SingleChildScrollView(
+                        scrollDirection: Axis.vertical,
+                        child: Text(context.watch<SelectText>().Text),
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        Navigator.pop(context, _selectText.Text);
+                      },
+                      child: const Text("OK"),
                     ),
                   ],
                 ),
-                Expanded(
-                  child: Container(
-                    margin: const EdgeInsets.all(10),
-                    color: appTheme(context).mColor1,
-                    height: double.infinity,
-                    width: double.infinity,
-                    child: WebView(),
-                    // child: IFrameScreen(
-                    //   url: url ?? "",
-                    // ),
-                  ),
-                ),
-                TextButton(
-                  onPressed: () {
-                    if (urlInput != null) {
-                      setState(() {
-                        url = urlInput;
-                      });
-                    }
-                  },
-                  child: const Text("OK"),
-                ),
-              ],
-            ),
-          ),
-        ],
+              ),
+            ],
+          );
+        },
       ),
     );
   }
 }
-
-// class IFrameScreen extends StatefulWidget {
-//   final String url;
-//   const IFrameScreen({super.key, required this.url});
-
-//   @override
-//   State<IFrameScreen> createState() => _IFrameScreenState();
-// }
-
-// class _IFrameScreenState extends State<IFrameScreen> {
-//   final IFrameElement _iFrameElement = IFrameElement();
-
-//   @override
-//   void initState() {
-//     _iFrameElement.style.height = '100%';
-//     _iFrameElement.style.width = '100%';
-//     _iFrameElement.src =
-//         "https://ranobelib.me/wujie-shushi-novel?section=comments";
-//     _iFrameElement.id = "HTML_View";
-//     _iFrameElement.style.border = 'none';
-//     ;
-
-//     // ignore: undefined_prefixed_name
-//     ui.platformViewRegistry.registerViewFactory(
-//       'HTML_IFrame',
-//       (int viewId) => _iFrameElement,
-//     );
-
-//     super.initState();
-//   }
-
-//   final Widget _iframeWidget = HtmlElementView(
-//     onPlatformViewCreated: (id) {
-//       print(document.getElementById('HTML_View'));
-//     },
-//     viewType: 'HTML_IFrame',
-//     key: UniqueKey(),
-//   );
-
-//   @override
-//   Widget build(BuildContext context) {
-
-//     // _iFrameElement.src = widget.url;
-
-//     return _iframeWidget;
-//   }
-// }
 
 class WebView extends StatefulWidget {
   const WebView({super.key});
@@ -142,20 +153,32 @@ class WebView extends StatefulWidget {
 }
 
 class _WebViewState extends State<WebView> {
-  late WebViewXController webviewController;
-
-  String data = "Test";
+  WebViewXController? webviewController;
 
   @override
   Widget build(BuildContext context) {
-    print(data);
+    // () async {
+    //   var response = await http.Client()
+    //       .get(Uri.parse('https://ranobelib.me/wujie-shushi-novel/v1/c1'));
+    //   if (response.statusCode == 200) {
+    //     String htmlToParse = response.body;
+    //     print(htmlToParse);
+    //     print('not error');
+    //   } else {
+    //     print("error");
+    //   }
+    // };
+
+    String url = context.watch<SelectUrl>().Url;
+    webviewController?.loadContent(url, SourceType.urlBypass);
 
     return WebViewAware(
       child: SizedBox(
         child: WebViewX(
+          // initialContent: 'https://ranobelib.me/wujie-shushi-novel/v1/c1',
           initialContent: 'https://flutter.dev',
           initialSourceType: SourceType.urlBypass,
-          onWebViewCreated: (controller) {
+          onWebViewCreated: (controller) async {
             webviewController = controller;
           },
           javascriptMode: JavascriptMode.unrestricted,
@@ -163,33 +186,25 @@ class _WebViewState extends State<WebView> {
             DartCallback(
               name: 'SetTextDart',
               callBack: (text) {
-                setState(() {
-                  data = text;
-                });
+                context.read<SelectText>().Text = text;
               },
             )
           },
           jsContent: const {
             EmbeddedJsContent(
-              // js: 'var select = true;' +
-              //     'var element = null;' +
-              //     'document.addEventListener(\'mouseover\', function(e) {if (select) {console.log(e.target); e.target.style.border = "thick solid #FF0000"; console.log(e.target.innerText);}});' +
-              //     'document.addEventListener(\'mouseout\', function(e) {if (select) {console.log(e.target); e.target.style.border = null;}});' +
-              //     'document.addEventListener(\'click\', function(e) {if (select) {console.log(e.target); e.target.style.border = "thick solid #00FF00"; select = false; element = e.target;}' +
-              //     ' else {select = true; element.style.border = null; element = null;}});',
               js: 'var select = true;' +
                   'var element = null;' +
                   'document.addEventListener(\'mouseover\', function(e) {if (select) { e.target.style.border = "thick solid #FF0000";}});' +
                   'document.addEventListener(\'mouseout\', function(e) {if (select) { e.target.style.border = null;}});' +
                   'document.addEventListener(\'click\', function(e) {if (select) { e.target.style.border = "thick solid #00FF00"; select = false; element = e.target; SetTextDart(element.innerText);}' +
-                  ' else {select = true; element.style.border = null; element = null;}});',
+                  ' else {select = true; element.style.border = null; element = null; SetTextDart("");}});',
             ),
           },
           height: MediaQuery.of(context).size.height,
           width: MediaQuery.of(context).size.width,
-          onPageFinished: (src) async {
-            final res = await webviewController.evalRawJavascript(
-                "window.document.getElementsByTagName('html')[0].innerHTML;");
+          onPageFinished: (src) {
+            // final res = await webviewController.evalRawJavascript(
+            //     "window.document.getElementsByTagName('html')[0].innerHTML;");
 
             // print(res);
           },
